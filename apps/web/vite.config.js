87d30cee970620cf4b014dc8849ab56927661afb
laakri <glassisaif@gmail.com>
2025-06-02 11:55:50 +0100

    (FIX) fixing the docker file for the web and remove the unneeded code

---
+++ apps/web/vite.config.js
@@ -0,0 +1,39 @@
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        watch: {
+            usePolling: true,
+            interval: 100,
+        },
+        hmr: {
+            overlay: true,
+            port: 24678,
+        },
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    optimizeDeps: {
+        include: [
+            'socket.io-client',
+            'engine.io-parser',
+            'engine.io-client'
+        ]
+    },
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+        commonjsOptions: {
+            include: [/node_modules/]
+        }
+    },
+    define: {
+        'process.env': process.env
+    },
+    envPrefix: 'VITE_'
+});
